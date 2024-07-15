07f72e17883329ec9b057ed7252583e206c4b9fc
sgrex <155350581+sgrex@users.noreply.github.com>
2024-07-16 00:46:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,4 @@
 document.getElementById('theme-toggle').addEventListener('click', function() {
     // Toggle dark/light mode
     document.body.classList.toggle('dark-mode');
-    document.body.classList.toggle('light-mode');
 });
-
-// Set initial theme based on
