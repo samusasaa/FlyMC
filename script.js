97f54680b51c269287d9d90cb4e23e9cdd5eb289
sgrex <155350581+sgrex@users.noreply.github.com>
2024-07-16 00:39:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to join button
+document.querySelector('button').addEventListener('click', function() {
+    // Open Minecraft client with FlyMC server details
+    window.open('minecraft://flymc.slicehosting.com:25565', '_blank');
+});
