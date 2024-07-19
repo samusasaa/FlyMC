ff9ad28debd5ffce4dca318405fb207e8ccd226e
sgrex <155350581+sgrex@users.noreply.github.com>
2024-07-19 22:23:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to theme toggle button
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    // Toggle dark/light mode
-    document.body.classList.toggle('dark-mode');
-});
